1e55325ef28f1008702d874e76f63d251996f950
Fanajaval <andriatoavimanana@gmail.com>
2026-08-17 17:30:49 +0300

    couleurs

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //principales
+  static const Color primary = Color(0xFF0D5C75);
+  static const Color accent = Color(0xFFFF6B4A);
+  static const Color nature = Color(0xFF2E7D32);
+
+  //background et texte
+  static const Color background = Color(0xFFF8F9FA);
+  static const Color textPrimary = Color(0xFF1E293B);
+
+  //carte
+  static const Color mapBackground = Color(0xFFF2F4F7);
+  static const Color mapWater = Color(0xFFD4E6F1);
+  static const Color mapGreen = Color(0xFFE8F5E9);
+
+  //neutres
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color grey = Color(0xFF64748B);
+  static const Color lightGrey = Color(0xFFE2E8F0);
+
+  // États
+  static const Color success = Color(0xFF2E7D32);
+  static const Color error = Color(0xFFDC2626);
+}
